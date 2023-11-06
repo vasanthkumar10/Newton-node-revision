40b0880ad4265304d369b782cbf6f5e3a4f102fe
Vasanthkumar Vinayagam <vasizebron@192.168.1.3>
2023-11-06 20:25:03 +0530

    validation

--- routes/cars.js
+++ routes/cars.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express();
 
+const Joi = require("joi");
+
 router.get("/cars", (req, res) => {
   console.log("returning cars");
   let cars = ["audi", "bmw"];
@@ -13,6 +15,16 @@ router.get("/cars", (req, res) => {
 router.post("/cars", (req, res) => {
   console.log("returning post cars");
   let cars = ["audi", "bmw"];
+
+  // define schema
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(15).required(),
+  });
+
+  const result = schema.validate(req.body);
+  // console.log(result);
+  if (result.error)
+    return res.status(400).send(result.error.details[0].message);
   return res.json({
     status: 200,
     data: cars,
